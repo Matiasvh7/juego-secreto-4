e52369f6046fb96a027ab49d7304c4cb83c05662
Your Name <matiasvh7@gmail.com>
2025-02-09 19:45:03 -0300

    actualizando

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el prob
 let listaDeParticipantes = [];
+let CambioDeTitulo = document.getElementById("amigo");
+let listaDeAmigos = document.getElementById("listaAmigos");
 
+log.console(CambioDeTitulo);
+log.console(listaDeAmigos);
 log.console(listaDeParticipantes);
 
 
